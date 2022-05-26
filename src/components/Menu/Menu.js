e5d5274bd160cb9fcdb978fc87a92660c7d29ba1
TheTT <phonvan128@gmail.com>
2022-05-26 20:49:46 +0700

    master

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Menu.scss';
+
+export default function Menu() {
+  return (
+    <div className='Menu'>
+      <ul className='list'>
+        <li>Home</li>
+        <li>Services</li>
+        <li>News</li>
+        <li>Blogs</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
